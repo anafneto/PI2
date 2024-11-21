71c59bc6a50c595b45f42313a3484128bfd4b24d
Renato Sobral <renato.sobral2005@gmail.com>
2024-11-21 17:36:47 +0000

    organização e butoes

--- js/general.js
+++ js/general.js
@@ -1,3 +1,13 @@
 function changeContent(newContent) {
     document.getElementById('changethis').innertext = newContent;
 }
+
+
+function hideContent() {
+    document.getElementById('content').style.visibility = 'hidden';
+}
+
+
+function hideContent() {
+    document.getElementById('content').style.visibility = 'hidden';
+}
