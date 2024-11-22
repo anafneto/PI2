5d4dd1784ce36739018de9e12e7771fc928dddb7
Black Seal <hugosfiuza@gmail.com>
2024-11-22 12:08:42 +0000

    Buttons CSS & JS

--- js/general.js
+++ js/general.js
@@ -11,3 +11,25 @@ function hideContent() {
 function hideContent() {
     document.getElementById('content').style.visibility = 'hidden';
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.date-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            selectDate(button); // Call selectDate to handle button selection
+        });
+    });
+});
+
+function selectDate(element) {
+    // Remove 'selected' class from all buttons
+    var buttons = document.querySelectorAll('.date-button');
+    buttons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+
+    // Add 'selected' class to the clicked button
+    element.classList.add('selected');
+}
